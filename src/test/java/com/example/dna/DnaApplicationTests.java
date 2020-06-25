0db1318c31a5bd22d119c810cc363c8265cd385f
CristianH21 <crishhtona21@gmail.com>
2020-06-24 23:35:32 -0500

    first commit

---
+++ src/test/java/com/example/dna/DnaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.dna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaApplicationTests {
+	
+
+	@Test
+	void contextLoads() {
+	}
+
+}
